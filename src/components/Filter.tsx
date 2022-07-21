9ef00562f8914b628b42df0d080486f5f6663ab5
Jonathan <jonathan.reis.torres@gmail.com>
2022-07-21 13:14:58 -0300

    'rockethelp'

---
+++ src/components/Filter.tsx
@@ -0,0 +1,31 @@
+import { Text, Button, IButtonProps, useTheme } from "native-base";
+
+type Props = IButtonProps & {
+  title: string;
+  isActive?: boolean;
+  type: "open" | "closed";
+};
+
+export function Filter({ title, isActive = false, type, ...rest }: Props) {
+  const { colors } = useTheme();
+  const colorType = type === "open" ? colors.secondary[700] : colors.green[300];
+  return (
+    <Button
+      size="sm"
+      flex={1}
+      bgColor="gray.600"
+      variant="outline"
+      borderWidth={isActive ? 1 : 0}
+      borderColor={colorType}
+      {...rest}
+    >
+      <Text
+        color={isActive ? colorType : "gray.300"}
+        fontSize="xs"
+        textTransform="uppercase"
+      >
+        {title}
+      </Text>
+    </Button>
+  );
+}
